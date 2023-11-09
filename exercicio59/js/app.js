77f740df6af6618ee7b2ff176d4bf54480190122
Gabriel Mioni <gabrielmionibastos@gmail.com>
2023-11-09 19:47:22 -0300

    Correção texto

--- exercicio59/js/app.js
+++ exercicio59/js/app.js
@@ -3,7 +3,7 @@ let number = parseInt(prompt("Digite um número"));
 let resultado = number % 2 == 0 ? 'Par' : 'Ímpar';
 
 if ( number <= 0 ) {
-    alert("O número digitado não pode ser")
+    alert("O número digitado não pode ser negativo")
 } else {
     alert(resultado)
 }
